a9b629a342b063301c3d4bace35f48d806ccda03
Betty H <101302743+bh-codes@users.noreply.github.com>
2022-11-10 22:03:24 -0500

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Main() {
+  return <h1>Main info here</h1>;
+}
+
+export default Main;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="nav">
+      <img src="../react-icon.png" alt="React Logo" />
+      <h3 className="react-facts">ReactFacts</h3>
+      <h4 className="extra-title">React Course - Project 1</h4>
+    </nav>
+  );
+}
+
+export default Navbar;
